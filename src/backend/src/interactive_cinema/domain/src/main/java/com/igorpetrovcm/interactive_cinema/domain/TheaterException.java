7c5901ab7d2420c7f076f502c2307f331ccc14cf
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-10-20 18:00:02 +0000

    Write domain prototype

---
+++ src/backend/src/interactive_cinema/domain/src/main/java/com/igorpetrovcm/interactive_cinema/domain/TheaterException.java
@@ -0,0 +1,8 @@
+package com.igorpetrovcm.interactive_cinema.domain;
+
+public class TheaterException
+    extends Exception {
+    public TheaterException(String message){
+        super(message);
+    }
+}
