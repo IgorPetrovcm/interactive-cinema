5f93892e82cc92cbe89429f1410d015be8da40b3
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-10-31 21:01:58 +0300

    Write CheckpointServiceException and Update FlowRunner

--- src/backend/src/interactive_cinema/usecase/src/main/java/com/igorpetrovcm/interactive_cinema/usecase/port/CheckpointService.java
+++ src/backend/src/interactive_cinema/usecase/src/main/java/com/igorpetrovcm/interactive_cinema/usecase/port/CheckpointService.java
@@ -1,7 +1,7 @@
 package com.igorpetrovcm.interactive_cinema.usecase.port;
 
 public interface CheckpointService{
-    void let();
+    void let() throws CheckpointServiceException;
 
-    void let(int sleepTime);
+    void let(int sleepTime) throws CheckpointServiceException;
 }
